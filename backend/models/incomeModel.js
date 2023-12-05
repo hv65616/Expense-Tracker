683454d172ab596a5be2c4878badd5477ca306de
Himanshu <hv65616@gmail.com>
2023-12-06 00:26:57 +0530

    income model created

---
+++ backend/models/incomeModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const incomeschema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+    type: {
+      type: String,
+      default: "income",
+    },
+    data: {
+      type: Date,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+    description: {
+      type: Date,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("Income", incomeschema);
