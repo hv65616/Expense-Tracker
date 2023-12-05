75a77a891bff62053001d429fe763f9b0c13bc7e
Himanshu <hv65616@gmail.com>
2023-12-06 00:26:27 +0530

    transaction route

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const db = require("../backend/db/db");
+const { readdirSync } = require("fs");
 dotenv.config();
 
 const app = express();
@@ -9,10 +10,15 @@ const PORT = process.env.PORT || 3000;
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+// database
 db();
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello" });
+
+// routes
+readdirSync("./routes").map((route) => {
+  app.use("/api/v1", require("./routes/transactions"));
 });
+
 app.listen(PORT, (req, res) => {
   console.log(`Server is running on port ${PORT}`);
 });
