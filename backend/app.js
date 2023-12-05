0dfbd38c5cdfe606cc03fb614d71237dd40caa6b
Himanshu <hv65616@gmail.com>
2023-12-05 23:19:12 +0530

    db import

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const db = require("../backend/db/db");
 dotenv.config();
 
 const app = express();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 // middlewares
 app.use(express.json());
 app.use(cors());
+db();
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello" });
 });
