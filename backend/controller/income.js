ee73cbbe77f05ca1c3cb8e2d3a94d7da6f330805
Himanshu <hv65616@gmail.com>
2023-12-07 00:18:21 +0530

    addincome and getincome controller implemented

--- backend/controller/income.js
+++ backend/controller/income.js
@@ -1,4 +1,37 @@
+const Income = require("../models/incomeModel");
 const addincome = async (req, res) => {
-  console.log(req.body);
+  const { title, amount, category, description, date } = req.body;
+  const income = Income({
+    title,
+    amount,
+    category,
+    description,
+    date,
+  });
+  try {
+    if (!title || !category || !description || !date) {
+      return res.status(400).json({ msg: "Please fill all fields" });
+    }
+    if (amount <= 0 || amount === "number") {
+      return res.status(400).json({ msg: "Amount must be a postitive number" });
+    }
+    await income.save();
+    res.status(200).json({
+      status: "success",
+      message: "Income data saved successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error From Add Income" });
+  }
+  console.log(income);
 };
-module.exports = { addincome };
+
+const getincome = async (req, res) => {
+  try {
+    const income = await Income.find().sort({ createdAt: -1 });
+    res.status(200).json(income);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error From Get Income" });
+  }
+};
+module.exports = { addincome,getincome };
