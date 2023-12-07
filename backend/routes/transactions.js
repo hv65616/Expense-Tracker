5b6398af064ccbe64558c28c683df47904235e34
Himanshu <hv65616@gmail.com>
2023-12-07 12:39:56 +0530

    expense route created

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,4 +1,10 @@
 const router = require("express").Router();
-const {addincome} = require("../controller/income");
+const {addincome, getincome, deleteincome} = require("../controller/income");
+const {addexpense, getexpense, deleteexpense} = require("../controller/expense")
 router.post("/add-income", addincome);
+router.get("/get-income",getincome);
+router.delete("/delete-income/:id",deleteincome)
+router.post("/add-expense",addexpense)
+router.get("/get-expense",getexpense)
+router.delete("/delete-expense/:id",deleteexpense);
 module.exports = router;
