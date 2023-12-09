ffbbd7bd18fd937bcf842eaf8d2ffec2887dab1f
Himanshu <hv65616@gmail.com>
2023-12-09 17:20:42 +0530

    styles for layout and main

---
+++ frontend/src/styles/Layouts.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+export const MainLayout = styled.div`
+  padding: 2rem;
+  display: flex;
+  height: 100%;
+  gap: 2rem;
+`;
+
+export const InnerLayout = styled.div`
+  padding: 2rem 1.5rem;
+  width: 100%;
+`;
