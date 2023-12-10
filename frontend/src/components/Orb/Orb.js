801a0219614cd0bcd4f33983a2b247298e055e29
Himanshu <hv65616@gmail.com>
2023-12-10 21:41:12 +0530

    orb file for animation

--- frontend/src/components/Orb/Orb.js
+++ frontend/src/components/Orb/Orb.js
@@ -1,6 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import { keyframes } from "styled-components";
+import { useWindowSize } from "../../utils/useWindowSize";
+
 function Orb() {
+  const { width, height } = useWindowSize();
+  // console.log(width,height);
+  const moveOrb = keyframes`
+    0%{
+      transform: translate(0,0);
+    }
+    50%{
+      transform: translate(${width/1.5}px,${height/1.5}px);
+    }
+    100%{
+      transform: translate(0,0);
+    }
+  `;
   const OrbStyled = styled.div`
     width: 70vh;
     height: 70vh;
@@ -9,7 +25,8 @@ function Orb() {
     margin-left: -37vh;
     margin-right: -37vh;
     background: linear-gradient(180deg, #f56692 0%, #f2994a 100%);
-    filter: blur(500.5px);
+    filter: blur(400.5px);
+    animation: ${moveOrb} 15s alternate linear infinite;
   `;
   return <OrbStyled></OrbStyled>;
 }
