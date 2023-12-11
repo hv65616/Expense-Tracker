c7cb856d971d828db39eab615dd8ea1383131651
Himanshu <hv65616@gmail.com>
2023-12-11 14:59:43 +0530

    components created

---
+++ frontend/src/components/Incomes/Incomes.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "styled-components";
+function Incomes() {
+  return <IncomesStyled>Incomes</IncomesStyled>;
+}
+const IncomesStyled = styled.div``;
+export default Incomes;
